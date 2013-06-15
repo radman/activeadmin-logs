5b443a8e59168924cd28de86d40651e24a81a440
Radu Vlad <radu.a.vlad@gmail.com>
2013-06-14 21:04:39 -0700

    adding controller action and sidebar

--- app/admin/logs.rb
+++ app/admin/logs.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register_page "Logs" do
+  page_action :index do
+    @logs = Dir[Rails.root.join("log/*.log")].map do |log|
+      File.basename(log, ".log")
+    end
+
+    @log = params[:log] || @logs.first
+  end
+
+  sidebar "Logs", :partial => "sidebar"
+  
   content do
     panel "Log" do
       "radu was here"
